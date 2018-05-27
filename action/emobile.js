a68418316424bc527b9fc8b0714f94b17fd9dfa8
kamineko2016 <kamineko2016@gmail.com>
2018-05-27 17:39:12 +0800

    setEmoto api

--- action/emobile.js
+++ action/emobile.js
@@ -42,3 +42,52 @@ export function getProfitValue() {
     console.log("caught: ", err);
   });
 }
+
+export function setEmotoInfomation(_emotoAddress, _plate, _driverName, _driverAddress) {
+  
+  let block = web3.eth.getBlock("latest");
+  let data = '0x';
+  // 取前2-10位（共8位數、省略0x)
+  let method = web3.sha3('setEmotoInfomation(address,bytes32,bytes32,address)').slice(2,10);
+  
+  let emotoAddress = _emotoAddress;
+  let plate = web3.toHex(_plate);
+  let driverName = web3.toHex(_driverName);
+  let driverAddress = _driverAddress;
+
+  let params =  transaction.to64Bytes(emotoAddress.slice(2,)) +
+                transaction.to64Bytes(plate.slice(2,)) +
+				transaction.to64Bytes(driverName.slice(2,)) +
+                transaction.to64Bytes(driverAddress.slice(2,));
+  console.log(params);
+  
+  
+
+  // 將 data 轉成 heximal格式
+  data += method + params;
+
+  let gasPrice = web3.eth.gasPrice;
+  let gasLimit = 1000000;
+  let value_ = 0x0;
+  let nonce = web3.eth.getTransactionCount(config.account);
+
+  // if gave the wrong address for to... no error pop on the terminal........
+  let txInfo = {
+      // from: config.account,
+      nonce: web3.toHex(nonce),
+      gasPrice: web3.toHex(gasPrice),
+      gasLimit: web3.toHex(gasLimit),
+      to: contractAddress.Emobile,
+      value: '0x00',
+      data: data,
+      chainId: 1
+  }
+  console.log(txInfo);
+
+  transaction.sendTransaction(txInfo).then(function(txhash) {
+    console.log('====setEmoto hash====');
+    console.log(txhash);
+  }).catch(err => {
+    console.log("caught: ", err);
+  });
+}
---
+++ route/emobile.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var apiRouter = express.Router();
+import web3 from '../test';
+
+import * as emobile from '../action/emobile';
+
+//好像沒用到的變數
+let driverContract;
+
+apiRouter.post('/setEmoto/', async function (req, res) {
+  try {
+    var emotoAddress = req.body.emoto;
+    var plate = req.body.plate;
+    var driverName = req.body.name;
+	var driverAddress = req.body.driver;
+    
+    console.log("set emoto api");
+	
+    var result = emobile.setEmotoInfomation(emotoAddress, plate, driverName, driverAddress);
+    console.log(result);
+    res.send(result);
+  } catch (e) {
+    res.status(404).send({
+      message: 'Not Found'
+    });
+  }
+});
+
+module.exports = apiRouter;
