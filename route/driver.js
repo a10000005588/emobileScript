269f1cb6de56a910f47fc731de57039d2f36f269
VIPLAB <viplab@VIPLABde-MacBook-Pro.local>
2018-05-31 17:05:26 +0800

    off-chain api finished

--- route/driver.js
+++ route/driver.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var apiRouter = express.Router();
+var request = require('request');
 import web3 from '../test';
 
 import * as driver from '../action/driver';
@@ -55,3 +56,25 @@ apiRouter.post('/:hash/credit', async function (req, res) {
     });
   }
 });
+
+request.post(
+    'http://140.113.63.172:5678/emobile/api.php',
+    { form: { "method": 'get_user_travel', 
+              "user": '0x0100000000000000000000000000000000000001'} },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    }
+);
+
+request.post(
+    'http://140.113.63.172:5678/emobile/api.php',
+    { form: { "method": 'get_driver_travel', 
+              "driver": '0x0100000000000000000000000000000000000001'} },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    }
+);
